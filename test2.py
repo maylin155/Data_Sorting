c59edce6b0fd1e427ecca122ccacd136ecfcb745
maylin155 <maylinlattmoe123@gmail.com>
2023-09-25 18:53:31 +0800

    split data

--- test2.py
+++ test2.py
@@ -14,16 +14,17 @@ def splitData(txt, splitstr):
     x = txt.split(splitstr)
     return x
 
+
+
 folder_path = input("Enter the folder path where raw data is located: ")
 if os.path.exists(folder_path):
     df = pd.read_csv(folder_path)
-    print(splitData((df['Name']),"_"))
-
-    #if __name__== "__main__":
-    txt = df['Name']
-    moduleList = splitData(txt, "_")
-    module = Module(moduleList[3])
-    print(module.name)
-    sessionList = splitData(moduleList[4],"/")
-    session = Session(sessionList[0])
-    print(session.name)
+    for i in range(len(df)):
+        value = df.loc[i, 'Name']
+        split_value = splitData(value, "_")
+        module = Module(split_value[3])
+        sessionList = splitData(split_value[4],"/")
+        session = Session(sessionList[0])
+        print(module.name)
+        print(session.name)
+        i += 1
