d7fc02643ac4d2850803367c8d1007026cbef34e
maylin155 <maylinlattmoe123@gmail.com>
2023-09-26 22:32:36 +0800

    module and session added to the dictionary

--- test2.py
+++ test2.py
@@ -9,72 +9,71 @@ class Session():
     def __init__(self,name):
         self.name = name
 
-
 def splitData(txt, splitstr):
     x = txt.split(splitstr)
     return x
 
-folder_path = input("Enter the folder path where raw data is located: ")
-if os.path.exists(folder_path):
-    df = pd.read_csv(folder_path)
+#Function to create module variables
+def create_module_instances(df):
+    module_variables = {}
+    for i, row in df.iterrows():
+        name = row['Name']
+        split_value = splitData(name, "_")
+        module_name = split_value[3]
+        module_variables[f"module{i}"] = Module(module_name)
+    return module_variables
 
-    # Initialize dictionaries to store module and session instances
-    Module_variables = {}
-    Session_variables = {}
+#Function to create session variables 
+def create_session_instances(df):
+    session_variables = {}
+    for i, row in df.iterrows():
+        name = row['Name']
+        split_value = splitData(name, "_")
+        session_list = splitData(split_value[4], "/")
+        session_name = session_list[0]
+        session_variables[f"session{i}"] = Session(session_name)
+    return session_variables
 
-    
-    # Iterate over the DataFrame
-    for i in range(len(df)):
-        value = df.loc[i, 'Name']
-        split_value = splitData(value, "_")
+#Function to drop "Name" column and add "Module" and "Session"
+def process_df(df, module_variables, session_variables):
+    # Remove the 'Name' column
+    df.drop(columns=['Name'], inplace=True)
 
-        # Create and store Module instances
-        Module_variables["module{0}".format(i)] = Module(split_value[3])
-        sessionList = splitData(split_value[4],"/")
+    # Add Module and Session information to the DataFrame
+    df['Module'] = [module_variables[f"module{i}"].name for i in range(len(df))]
+    df['Session'] = [session_variables[f"session{i}"].name for i in range(len(df))]
 
-        # Create and store Session instances
-        Session_variables["session{0}".format(i)] = Session(sessionList[0])
-    
-    #print(Module_variables)
-    #print(Session_variables)
-    
+#Iterate through the folder and add the excel files into a list.
+def list_files(folder_path):
+    excel_dict_list= []
+    excel_files = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
+    for excel_file in excel_files:
+        file_path = os.path.join(folder_path, excel_file)
+        df = pd.read_csv(file_path)
+        module_variables = create_module_instances(df)
+        session_variables = create_session_instances(df)
+        process_df(df, module_variables, session_variables)
+        dict = df.to_dict(orient= 'index')
+        excel_dict_list.append((excel_file, dict))
+    return excel_dict_list
 
-    #df = df.drop(columns=['Name'])
-    # Iterate over Module and Session dictionaries to access their attributes
 
+def main():
+    folder_path = input("Enter the folder path where raw data is located: ")
+    if os.path.exists(folder_path):
+        excel_dict_list = list_files(folder_path)
+        print(excel_dict_list[0][1][0]['Description'])
 
+        #excel_dict_list contains dictionaries from excel files in the folder.
+        #for excel_file, data in excel_dict_list:
+            #print(f"Data from Excel file: {excel_file}")
+            #for i, record in enumerate(data, start=1):
+                #print(f"Record {i}: {record}")
+            #print()
 
+    else:
+        print(f"The folder path '{folder_path}' does not exist.")
 
-    dict = df.to_dict(orient ='index')
-    #print(dict)
+if __name__ == "__main__":
+    main()
 
-    #Key to remove from each inner dictionary
-    key = 'Name'
-
-    #Iterate through outer dictionary
-    for index, value in dict.items():
-        #Remove the key if it exists in inner dictionary
-        if key in value:
-            value.pop(key)
-    
-    for x in Module_variables.values():
-        #Get key, value pairs to add to inner dictionary.
-        new_key = 'Module'  
-        new_module = x.name
-
-        #Iterate through outer dictionary
-        for index, value in dict.items():
-            #Adding the new key-value pair
-            value[new_key] = new_module
-
-    for y in Session_variables.values():
-        #Get key, value pairs to add to inner dictionary. 
-        new_key = 'Session'
-        new_session = y.name
-
-        #Iterate through outer dictionary
-        for index, value in dict.items():
-            #Adding the new key-value pair
-            value[new_key] = new_module
-    
-    print(dict)
