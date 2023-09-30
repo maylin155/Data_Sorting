0c48f10320b3c7fda58c0d13bb8bc9e59e5ba90d
maylin155 <maylinlattmoe123@gmail.com>
2023-09-30 18:48:22 +0800

    extract and create module and session column for one file

--- test3.py
+++ test3.py
@@ -7,16 +7,30 @@ def splitData(txt, splitstr):
     x = txt.split(splitstr)
     return x
 
-def create_module_row(df):
-    pass
+def extract_module_data(name):
+        split_value = splitData(name,"_")
+        if len(split_value) >= 4:
+            return split_value[3]
+        else:
+             return None
 
-def create_session_row(df):
-    pass
+def extract_session_data(name):
+    split_value = splitData(name, "_")
+    session_list = splitData(split_value[4], "/")
+    if len(session_list) >= 1:
+         return session_list[0]
+    else:
+         return None
 
 def main():
     file_path = input("Enter file path: ")
     df = pd.read_csv(file_path)
+    
+    df['Module'] = df['Name'].apply(extract_module_data)
+    df['Session'] = df['Name'].apply(extract_session_data)
+    
     df = remove_column(df)
+
     print(df)
 
 if __name__ == "__main__":
